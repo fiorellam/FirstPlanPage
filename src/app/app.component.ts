32228a160b73de97095f617fff19fca9229be15c
Ernesto Macfarland <ernesto,macfarland@gmail.com>
2019-05-04 23:22:55 -0700

    Home Component casi terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Career } from 'src/app/models/career';
-import { CareerViewModel } from 'src/app/models/career-view-model';
-import { ServiceDBService }  from 'src/app/services/service-db.service'; 
-import { DomSanitizer } from '@angular/platform-browser'
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,37 +8,9 @@ export class AppComponent implements OnInit  {
   title = 'FirstPlanAngular';
 
   ngOnInit(){
-    this.loadCareers();
   }
 
-  constructor(private careerService: ServiceDBService,
-  private sanitizer: DomSanitizer){
-    
-  }
-
-  careers : CareerViewModel[] = [];
-  loadCareers(){
-    this.careerService.getCareers().subscribe(response => {
-      this.careers = [];
-      response.docs.forEach( value => {
-        const data = value.data();
-        const id = value.id;
-        const career : CareerViewModel = {
-          id: id,
-          autor: data.autor,
-          color: data.color,
-          descripcion: data.descripcion,
-          imagen: data.imagen,
-          nombre: data.nombre,
-          texto: data.texto,
-          videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl(data.videoUrl)
-          
-        };
-        this.careers.push(career);
-        
-      })
-    })
-
+  constructor(){
   }
   
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { CareerViewModel } from 'src/app/models/career-view-model';
+import { ServiceDBService }  from 'src/app/services/service-db.service'; 
+import { DomSanitizer } from '@angular/platform-browser'
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +12,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  title = 'FirstPlanAngular';
+
+  ngOnInit(){
+    this.loadCareers();
   }
+
+  constructor(private careerService: ServiceDBService,
+  private sanitizer: DomSanitizer){
+    
+  }
+
+  careers : CareerViewModel[] = [];
+  loadCareers(){
+    this.careerService.getCareers().subscribe(response => {
+      this.careers = [];
+      response.docs.forEach( value => {
+        const data = value.data();
+        const id = value.id;
+        const career : CareerViewModel = {
+          id: id,
+          autor: data.autor,
+          color: data.color,
+          descripcion: data.descripcion,
+          imagen: data.imagen,
+          nombre: data.nombre,
+          texto: data.texto,
+          videoUrl: this.sanitizer.bypassSecurityTrustResourceUrl(data.videoUrl)
+          
+        };
+        this.careers.push(career);
+        
+      })
+    })
+
+  }
+  
 
 }
